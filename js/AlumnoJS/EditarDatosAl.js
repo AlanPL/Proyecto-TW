2139471108e7f76b0defb40b1cfe129d01ef22b6
Samuel Raya Chávez <43630037+samiraya23@users.noreply.github.com>
2020-06-12 19:32:48 -0500

    JSalumno

---
+++ js/AlumnoJS/EditarDatosAl.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+   $("#formEditarContrasenaAl").submit(function(e){
+        e.preventDefault();
+        var formulario = $("#formEditarContrasenaAl").serialize();
+        $.ajax({
+            type:"POST",
+            url:"../php/crudAlumno.php",
+            data: formulario,
+            cache:false,
+            success:function(respAX){
+
+                if (respAX==1) {    
+                    $.notify("Contraseña editada","success");
+                }else{
+                    $.notify("Algo salió mal... :/","error");
+                }
+            },
+            error: function(a) {
+              console.log('error:\n');
+              console.log(a);
+            }
+        });
+   })
+
+      $("#formEditarCorreoAl").submit(function(e){
+        e.preventDefault();
+        var formulario = $("#formEditarCorreoAl").serialize();
+        $.ajax({
+            type:"POST",
+            url:"../php/crudAlumno.php",
+            data: formulario,
+            cache:false,
+            success:function(respAX){
+
+                if (respAX==1) {    
+                    $.notify("Correo editado","success");
+                }else{
+                    $.notify("Algo salió mal...","error");
+                }
+            },
+            error: function(a) {
+              console.log('error:\n');
+              console.log(a);
+            }
+        });
+   })
+
+});
