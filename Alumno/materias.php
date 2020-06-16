ceac2620f7bb85d5543f769acfdd83a5dc6cd0e8
Samuel Raya Chávez <43630037+samiraya23@users.noreply.github.com>
2020-06-15 19:09:20 -0500

    Update materias.php

--- Alumno/materias.php
+++ Alumno/materias.php
@@ -7,7 +7,7 @@
         $correo=$_SESSION["correo"];
     }
     if( !isset($_SESSION["correo"])){
-        header("location:../login.html");
+        header("location:../login.php");
     }
 ?>
 <div id="wrapper">
